c5ea99da6a52d93fa20898423deb0ab709172acd
li-qiang <kata.Qlee@gmail.com>
2016-05-14 10:48:29 +0800

    Add selection sort

--- test/compare-spec.js
+++ test/compare-spec.js
@@ -7,8 +7,9 @@ let HeapSort = require('../sorts/heap-sort');
 let MergeSort = require('../sorts/merge-sort');
 let ShellSort = require('../sorts/shell-sort');
 let BubbleSort = require('../sorts/bubble-sort');
+let SelectionSort = require('../sorts/selection-sort');
 
-describe('Compare', () => {
+describe('@compare', () => {
 
     let randomArr = loopTimes(10000, () => 1 + (Math.random() * 1000).toFixed()),
         sortedArr = Object.assign([], randomArr).sort(),
@@ -40,9 +41,15 @@ describe('Compare', () => {
         expect(shellSorter.result).to.deep.equal(sortedArr);
     });
 
-    it('sort by bubble sort', () => {
+    it('sort by bubble sort @slow', () => {
         let bubbleSorter = new BubbleSort(originArr);
 
         expect(bubbleSorter.result).to.deep.equal(sortedArr);
+    });
+
+    it('sort by selection sort @slow', () => {
+        let selectionSorter = new SelectionSort(originArr);
+
+        expect(selectionSorter.result).to.deep.equal(sortedArr);
     });
 });
